97a8965556c348445e90a4cc1980014d50236e78
wael <waeelash155@gmail.com>
2022-04-04 10:54:14 -0400

    commit tree 25ccbffd5b6987d57d5d2201511c596ce3dd2786

---
+++ app.rb
@@ -0,0 +1,9 @@
+# app.rb
+puts "Hello World!"
+# puts "Hello world!"
+# puts "Hello world!"
+# print [1, 2, 3]
+# p [1, 2, 3]
+# pp [{ id: 1, hello: "World" }, { id: 2, hello: "Ruby" }, { id: 3, hello: "Moon" }, { id: 4, hello: "Learner" }]
+print "Pass this test, please."
+p [1, 2, 3]
